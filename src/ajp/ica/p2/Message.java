23f8742caebc9ea297638f6fd988363aa3b781cc
v8196566 <v8196566@linux40.scm.tees.ac.uk>
2019-12-04 11:37:00 +0000

    Getting rid of stupid default project comments.

--- src/ajp/ica/p2/Message.java
+++ src/ajp/ica/p2/Message.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ajp.ica.p2;
 
-/**
- *
- * @author v8196566
- */
 public class Message {
     final private String sender, receiver, content;
 
