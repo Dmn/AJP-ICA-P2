7a3beffdbc25bc99e11ca65cd79c60e2e15a94ac
v8196566 <v8196566@linux41.scm.tees.ac.uk>
2019-12-11 12:17:35 +0000

    Testing

--- src/ajp/ica/p2/MetaAgent.java
+++ src/ajp/ica/p2/MetaAgent.java
@@ -3,14 +3,22 @@ package ajp.ica.p2;
 import java.util.concurrent.ArrayBlockingQueue;
 
 
-public class MetaAgent extends ArrayBlockingQueue implements Runnable
+public class MetaAgent extends ArrayBlockingQueue<Message> implements Runnable
 {
     String name = "";
     Portal portal = null;
+    private Thread thread;
+    private volatile boolean run;
     
     public MetaAgent()
     {
         super(100);
+    }
+    
+    private void start()
+    {
+        thread = new Thread(this);
+        thread.start();
     }
 
     @Override
@@ -18,8 +26,9 @@ public class MetaAgent extends ArrayBlockingQueue implements Runnable
         Thread messageFling=new Thread(this);
     }
     
-    public synchronized void msgHandler(UserAgent receiver ,Message msg)
+    public synchronized void msgHandler(Message msg)
     {
+        
         //Handle the messages.
         //UserAgent r= receiver;
         //BlockingQueue q= r.getQueue();
