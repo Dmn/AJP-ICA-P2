552046981ff3f6e38babd83a6eaf5d0dac03ebde
v8196566 <v8196566@linux41.scm.tees.ac.uk>
2019-12-11 11:35:18 +0000

    Adding executor service

--- src/ajp/ica/p2/MessageQueue.java
+++ src/ajp/ica/p2/MessageQueue.java
@@ -1,13 +1,22 @@
 package ajp.ica.p2;
 
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+
 
 public class MessageQueue extends ArrayBlockingQueue
 {
+    static MessageQueue msgQ = null;
+    
     private final Message msg;
     MessageQueue(Message msg, int amt)
     {
         super(amt);
         this.msg = msg;
+    }
+    
+    static MessageQueue setMsgQ(MessageQueue msgQ)
+    {
+        return null;
     }
 }
