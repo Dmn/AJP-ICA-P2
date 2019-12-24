e3887568fe2f591055ba1646e335e69ffbc79e58
v8196566 <v8196566@live.tees.ac.uk>
2019-12-24 00:29:48 +0000

    Unit tests for the class Message added.

---
+++ test/ica/MessageTest.java
@@ -0,0 +1,58 @@
+package ica;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MessageTest {
+    
+    public MessageTest() {
+    }
+
+    /**
+     * Test of getSender method, of class Message.
+     */
+    @Test
+    public void testGetSender() {
+        System.out.println("getSender");
+        Message instance = new Message("Jason","Cristian","Hello World");
+        String expResult = "Jason";
+        String result = instance.getSender();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getReceiver method, of class Message.
+     */
+    @Test
+    public void testGetReceiver() {
+        System.out.println("getReceiver");
+        Message instance = new Message("Jason","Cristian","Hello World");
+        String expResult = "Cristian";
+        String result = instance.getReceiver();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getContent method, of class Message.
+     */
+    @Test
+    public void testGetContent() {
+        System.out.println("getContent");
+        Message instance = new Message("Jason","Cristian","Hello World");
+        String expResult = "Hello World";
+        String result = instance.getContent();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of wrap method, of class Message.
+     */
+    @Test
+    public void testWrap() {
+        System.out.println("wrap");
+        Message instance = new Message("Jason","Cristian","Hello World");
+        String expResult = "Jason:Cristian:Hello World";
+        String result = instance.wrap();
+        assertEquals(expResult, result);
+    }
+}
