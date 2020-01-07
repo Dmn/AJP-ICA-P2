7cbfc5bb1b28e58bf44a56ec1b98ada8edcad9b2
Ashley <ashle@DESKTOP-AKPBG4T>
2020-01-07 21:17:49 +0000

    >Added hashmap for socket connections >Added addSocket method >Added removeSocket method >gave socket a getName to use for former methods

--- src/ica/SocketAgent.java
+++ src/ica/SocketAgent.java
@@ -5,28 +5,34 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.HashMap;
 
 /**
  *
  * @author t7082305
  */
-
 // this is class method that extends to the metaagent
-public class SocketAgent extends MetaAgent{
-    
-    private Socket socket;
+public class SocketAgent extends MetaAgent {
 
-    public SocketAgent(String name, Portal portal,Socket socket) {
+    private Socket socket;
+    HashMap<String, SocketAgent> socketList = new HashMap<String, SocketAgent>();
+
+    public SocketAgent(String name, Portal portal, Socket socket) {
         super(name, portal);
         this.socket = socket;
     }
+
+    public String getName() {
+        return name;
+    }
+
     // this is while loop that reads the data input and thread
-    public void makeReadLoop(){
+    public void makeReadLoop() {
         Thread thread = new Thread(() -> {
             try {
                 DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
                 String input;
-                while((input=dataInputStream.readUTF())!=null){
+                while ((input = dataInputStream.readUTF()) != null) {
                     portal.msgQueue.add(textToMsg(input));
                 }
             } catch (IOException ex) {
@@ -37,21 +43,42 @@ public class SocketAgent extends MetaAgent{
     }
 
     private Message textToMsg(String input) {
-        return new Message("socket","portal",input);
+        return new Message("socket", "portal", input);
     }
 
     @Override
     // this is method  msgHandler if message has error or not
     public void msgHandler(Message msg) {
-        try{
+        try {
             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
             dataOutputStream.writeUTF(msgToText(msg));
-        }catch(IOException ex){
+        } catch (IOException ex) {
             System.out.println("Error");
         }
     }
 // this is the msgtotext that has content
+
     private String msgToText(Message msg) {
         return msg.getContent();
+    }
+/*
+    void routerConnect(Router aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+*/
+    private void addSocket(SocketAgent partner) {
+        if (!socketList.containsKey(partner.getName())) {
+            socketList.put(partner.getName(), partner);
+        }
+    }
+
+    private void removeSocket(SocketAgent partner) {
+        boolean socketDeleted;
+        if (socketList.containsKey(partner.getName())) {
+            socketDeleted = socketList.remove(partner.getName(), partner);
+        }
+        if (socketDeleted == false) {
+            System.out.println("Could not be deleted");
+        }
     }
 }
