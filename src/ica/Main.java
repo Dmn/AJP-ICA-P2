e962b0324d98dd3c51ba4d5155fd162ffc91bf23
Ashley <ashle@DESKTOP-AKPBG4T>
2020-01-08 23:02:59 +0000

    >Added JavaDoc to SocketAgent >Adjustments to my tests in Main

--- src/ica/Main.java
+++ src/ica/Main.java
@@ -142,115 +142,8 @@ public class Main {
         S2Sagent1.removeSocket(S2Sagent2);
         System.out.println("If the 2nd agent is deleted from the hash map, running again should print a error 'Could not be deleted'");
         S2Sagent1.removeSocket(S2Sagent2);
-<<<<<<< Upstream, based on origin/t7199384
-=======
-//        System.out.println("Testing adding Agents to one portal.");
-//
-//        Portal portal = new Portal("P1", null);
-//        UserAgent one = new UserAgent("Jason", portal);
-//        UserAgent two = new UserAgent("Cristian", portal);
-//        
-//        System.out.printf("Testing UserAgent: %s\n", one.toString());
-//        System.out.printf("Testing if Agent added to Routing table: %s\n", portal.getKeyAgent("Jason").getName());
-//        System.out.println("_______\n");
-//
-//        System.out.println("Testing adding Portals to new portals.");
-//
-//        Portal portalTwo = new Portal("P2", null);
-//        portalTwo.addPortal(portal);
-//
-//        System.out.printf("Portal one is connect to: %s\n", portal.getPortal().getName());
-//
-//        System.out.printf("Testing if Routing tables get merged: %s\n", portalTwo.routingTableToString());
-//
-//        UserAgent three = new UserAgent("Joshua", portalTwo);
-//        UserAgent four = new UserAgent("Jake", portalTwo);
-//
-//        Portal portalThree = new Portal("P3", portalTwo);
-//
-//        UserAgent five = new UserAgent("Drake", portalThree);
-//
-//        System.out.printf("Testing if Routing tables synced when new Agent: %s\n", portal.routingTableToString());
-//
-//        /**
-//        Portal one = new Portal("P1", null);
-//        UserAgent a1 = new UserAgent("A1", one);
-//        UserAgent a2 = new UserAgent ("A2", one);
-//
-//
-//        a1.sendMessage(new Message(a1.getName(),a2.getName(),"Hello"));
-//         */
-//
-//        System.out.println(portal.routingTableToString());
-//
-//        /** Removing an agent requires you to make method removeAgent return null if it succeeds or return agent. */
-//        //System.out.println("Testing if UserAgent gets removed from routing tables.");
-//        //three = portalTwo.removeAgent(three);
-//
-//        /** Code below should produce a NullPointerException because we null the UserAgent "three". */
-//        //try {
-//        //   System.out.printf("%s", three.toString());
-//        //}
-//        //catch (NullPointerException ex) {
-//        //    System.out.println("This UserAgent does not exist.");
-//        //}
-//
-//        Vector<UserAgent> agents = new Vector<>();
-//        agents.add(one);
-//        agents.add(two);
-//        agents.add(three);
-//        agents.add(four);
-//        agents.add(five);
-//
-//        //one.sendMessage(two.getName(),"Hello A2.");
-//        //two.sendMessage(three.getName(),"Hey there Joshua.");
-//
-//        for (UserAgent i: agents) {
-//            one.sendMessage(i.getName(), ("Spamming. Hello mr. " + i.getName()));
-//        }
-//        five.sendMessage(one.getName(), "What the hell.");
-//
-//        //System.out.printf("New Routing table: %s\n", portalTwo.routingTableToString());
-//
-//
-//
-//        //one.sendMessage(two, "Hello World");
-//
-//        //Agent three = new Agent("Josh");
-//        //Portal portalTwo = new Portal();
-//        //portalTwo.addAgent(three.getName(), three);
-//
-//        //Socket router = new Socket();
-//        //router.addPortal(portalOne);
-//        //router.addPortal(portalTwo);
-//
-//        //three.sendMessage(one, "Communicating through socket");
-//        //two.sendMessage(three, "Communicating through socket Part 2");
-//
-//        /**
-//         * Disclaimer: The method addAgent's parameters may not be correct,
-//         * This was my interpretation of the BORIS implementations of that method.
-//         */
-//
-//        /**
-//
-//         MessageQueue msgQ;
-//         msgQ = new MessageQueue("User1", 100);
-//
-//         msgQ.add(new Message("Hello Jason!"));
-//
-//         for(int i = 0; i < 40; i++)
-//         {
-//            msgQ.add(new Message("Hello " + i));
-//            Thread.sleep(100);
-//         }
-//
-//         msgQ.stop();
-//         System.out.println("Messages stopped.");
-//         Test
-//         */
-
-
+        System.out.println("\n\n");
+        
      Portal p1 = new Portal("p1",null);
      Portal p2 = new Portal("p2",null);
      Portal p3 = new Portal("p3",null);
@@ -268,7 +161,5 @@ public class Main {
      A4.sendMessage("A2", "YP");
      A3.sendMessage("A1", "YP");
      A2.sendMessage("A3", "YP");
-=======
->>>>>>> 93a0352 >Added hashmap for socket connections >Added addSocket method >Added removeSocket method >gave socket a getName to use for former methods >Added socket adding and removing to main method for testing
     }
 }
